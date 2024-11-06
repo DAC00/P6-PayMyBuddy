a7c7dab4d709c220a038ea89871e764fdb72e298
DAC00 <boiteaspamv2@gmail.com>
2024-11-06 17:15:07 +0100

    Signup controller & html

---
+++ src/main/java/com/opcr/payMyBuddy/controller/SignupController.java
@@ -0,0 +1,41 @@
+package com.opcr.payMyBuddy.controller;
+
+import com.opcr.payMyBuddy.exception.EmailAlreadyExistsException;
+import com.opcr.payMyBuddy.model.BuddyUser;
+import com.opcr.payMyBuddy.service.BuddyUserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignupController {
+
+    @Autowired
+    private BuddyUserService buddyUserService;
+
+    private static final Logger logger = LogManager.getLogger(SignupController.class);
+
+    @GetMapping("/signup")
+    public String signupView(Model model) {
+        model.addAttribute("buddyuser",new BuddyUser());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute("buddyuser") BuddyUser newBuddyUser, Model model){
+        try {
+            buddyUserService.addUser(newBuddyUser.getUsername(),newBuddyUser.getEmail(),newBuddyUser.getPassword());
+            logger.info("New user created : %s".formatted(newBuddyUser.getEmail()));
+            return "redirect:/login";
+        } catch (EmailAlreadyExistsException e) {
+            model.addAttribute("errorMessage", "Email already taken.");
+            logger.error(e.getMessage());
+            return "signup";
+        }
+    }
+}
